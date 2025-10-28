f7d45565c469b16aa800ad9d07c042e2568de6d9
Misaka24 <sgqy20070424@163.com>
2025-10-28 17:54:37 +0800

    Impletion: Add multi-conditional checks using "el se if" for handling numbers with more digits

--- C_25.10.27_assigment_2/src-v1/main.c
+++ C_25.10.27_assigment_2/src-v1/main.c
@@ -8,6 +8,10 @@ int main()
         printf("该数字是个位数。\n");
     else if(n>9&&n<100 || n<-9&&n>-100)
         printf("该数字是两位数。\n");
+    else if(n>99&&n<1000 || n<-99&&n>-1000)
+        printf("该数字是三位数。\n");
+    else if(n>999&&n<10000 || n<-999&&n>-10000)
+        printf("该数字是四位数。\n");
     else
         printf("该数字其他数。\n");
 }
